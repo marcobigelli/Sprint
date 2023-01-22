e888eb43c57fab1dbd0923d09948618746f16d19
marcobigelli <1618bi@gmail.com>
2023-01-22 04:15:38 -0300

    Subindo arquivos

---
+++ produtos.js
@@ -0,0 +1,44 @@
+let produtos = [
+    {
+        id:0,
+        nome: "Flores",
+        preco: 38.90,
+        image: "./img/Flores.png"
+    },
+    {
+        id:1,
+        nome: "Frutíferas",
+        preco: 32.90,
+        image: "./img/Frutíferas.png"
+    },
+    {
+        id:2,
+        nome: "Medicinais",
+        preco: 16.90,
+        image: "./img/Medicinais.png"
+    },
+    {
+        id:3,
+        nome: "Suculentas",
+        preco: 9.80,
+        image: "./img/Suculentas.png"
+    },
+    {
+        id:4,
+        nome: "Ornamentais",
+        preco: 13.90,
+        image: "./img/Ornamentais.png"
+    },
+    {
+        id:5,
+        nome: "Jardinagem",
+        preco: 38.90,
+        image: "./img/Jardinagem.png"
+    },
+ 
+    
+]
+
+
+      
+       
